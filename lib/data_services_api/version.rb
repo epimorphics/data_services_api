df6cb531c1f9a721773130fa37f38acb91774d3c
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-06-21 15:51:01 +0100

    build: Incremented Minor Version Cadence due to depth of changes

--- lib/data_services_api/version.rb
+++ lib/data_services_api/version.rb
@@ -3,8 +3,8 @@
 # :nodoc:
 module DataServicesApi
   MAJOR = 1
-  MINOR = 3
-  PATCH = 3
+  MINOR = 4
+  PATCH = 0
   SUFFIX = nil
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}"
 end
