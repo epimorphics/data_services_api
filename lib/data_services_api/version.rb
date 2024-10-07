6bd7bf2d9599f01a67e18d4314f8c114a8a9d7f3
Daniel Couzens <dan.couzens@epimorphics.com>
2024-10-07 16:07:23 +0100

    Data services api: ruby updrade

--- lib/data_services_api/version.rb
+++ lib/data_services_api/version.rb
@@ -3,8 +3,8 @@
 # :nodoc:
 module DataServicesApi
   MAJOR = 1
-  MINOR = 4
-  PATCH = 1
-  SUFFIX = 1
+  MINOR = 5
+  PATCH = 0
+  SUFFIX = 0
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}"
 end
