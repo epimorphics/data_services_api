27ea514fe85f90fe75b480b53f3c19d65faf924a
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-02-14 16:15:41 +0000

    test: Refactor query structure for clarity
    Updated the query format to improve readability by restructuring nested elements. This makes it easier to understand and maintain in future updates.

--- test/data_services_api/dataset_test.rb
+++ test/data_services_api/dataset_test.rb
@@ -56,7 +56,12 @@ describe 'DataServiceApi::Dataset' do
   it 'should accept a query and return the result' do
     query = Class.new do
       def terms
-        { '@and' => [{ 'ukhpi:refMonth' => { '@ge' => { :@value => '2019-01', :@type => 'http://www.w3.org/2001/XMLSchema#gYearMonth' } } }, { 'ukhpi:refRegion' => { '@eq' => { :@id => 'http://landregistry.data.gov.uk/id/region/united-kingdom' } } }], '@sort' => [{ '@down' => 'ukhpi:refMonth' }], '@limit' => 1 }
+        { '@and' => [
+          { 'ukhpi:refMonth' => { '@ge' => { :@value => '2019-01', :@type => 'http://www.w3.org/2001/XMLSchema#gYearMonth' } } },
+          { 'ukhpi:refRegion' => { '@eq' => { :@id => 'http://landregistry.data.gov.uk/id/region/united-kingdom' } } }
+        ], '@sort' => [
+          { '@down' => 'ukhpi:refMonth' }
+        ], '@limit' => 1 }
       end
 
       def to_json(*_args)
